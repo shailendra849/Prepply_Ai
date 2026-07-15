6befb6cc999f1739cc82dd1201a463447e72ee8d
Shailendra Dwivedi <shailendrad699@gmail.com>
2026-07-15 23:44:57 +0530

    Serve React frontend from public folder

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,17 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
-}))
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
@@ -18,11 +19,16 @@ const interviewRouter = require("./routes/interview.routes")
 const mockRouter = require("./routes/mock.routes")
 
 
+
+
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 app.use("/api/mock", mockRouter)
 
-
+// Serve React app for all non-API routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
 module.exports = app
